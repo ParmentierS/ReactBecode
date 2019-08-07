05eea5d389154097316584a18fb8d5f5f204f847
Sebastien Parmentier <sebastien.parmentier@mail.com>
2019-08-07 15:26:06 +0200

    7/8 15h25 happydrink almost finished

--- happydrink/src/App.js
+++ happydrink/src/App.js
@@ -6,13 +6,185 @@ import { establishments }     from './fixtures'
 
 class App extends React.Component 
 {
-  state =
+  constructor(props) 
   {
-    favorites : []
+    super(props);
+    this.state =
+    {
+      searchString : "",
+      favoritesBar : [],
+      renderlistFavorites: [],
+      filteredBar :establishments,
+      renderFilteredBar :[],
+      renderEstablishment :establishments.map( (establishment) => 
+      {
+        return (
+            // L'attribut "key" permet à React d'identifier les éléments.
+            // Cela améliore les performances lors de l'ajout,
+            // la modification et la suppression d'un élément.
+            <li
+                key = { establishment.id }
+                className = 'establishment'
+                onClick={this.changeFavorites}
+            >
+                <h3>{ establishment.name }</h3>
+  
+                { establishment.description }
+
+                <h3>Happy Hour</h3>
+  
+                { establishment.happyHour }
+  
+            </li>
+            
+        )
+      })
+    }
+    this.changeFavorites = this.changeFavorites.bind(this);
+    this.filterList = this.filterList.bind(this);
   }
+  filterList = async function(event)
+  {
+    await this.setState({searchString : event.target.value.toLowerCase()})
+    //console.log(this.state.searchString);
+    /*for(let key in event)
+    {
+      console.log(key," : ",event[key])
+    }
+    for(let key in event)
+    {
+      if(event[key])
+        console.log(key," : ",event[key]["data"])
+    }*/
+    console.table(this.state.renderEstablishment);
+    await this.setState({filteredBar: establishments.filter( 
+      (establishment) => {
+        
+        let str = establishment.name + " " + establishment.description;
+        str=str.toLowerCase();
+        console.log(str);
+        return str.match(this.state.searchString);
+        
+      })})
+
+    await this.setState({renderFilteredBar: this.state.filteredBar.map( (establishment) => 
+      {
+        return (
+            // L'attribut "key" permet à React d'identifier les éléments.
+            // Cela améliore les performances lors de l'ajout,
+            // la modification et la suppression d'un élément.
+            <li
+                key = { establishment.id }
+                className = 'establishment'
+                onClick={this.changeFavorites}
+            >
+                <h3>{ establishment.name }</h3>
+  
+                { establishment.description }
+
+                <h3>Happy Hour</h3>
+  
+                { establishment.happyHour }
+  
+            </li>
+            
+        )
+      })})
+      console.log(this.state.searchString);
+      console.table(this.state.filteredBar);
+      console.table(this.state.renderFilteredBar);
+  }
+  changeFavorites = function(event)
+    {
+      /*_targetInst or _dispatchInstances 
+      it seems like targetInst doesn't work each time*/
+      /*for(let key in event)
+        {
+          console.log(key," : ",event[key])
+        }
+      for(let key in event)
+        {
+          if(event[key])
+            console.log(key," : ",event[key]["key"])
+        }*/
+      const key = event._dispatchInstances["key"]
+      let  estab = {};
+      let alreadyFavorited = false;
+      let indexFavorited=0;
+      const myFavorites = this.state.favoritesBar;
+      for (let e of establishments)
+      {
+        console.log(e.id, key)
+        if(e.id === key)
+        {
+          estab=e;
+        }
+      }
+      for (let keyBar in myFavorites)
+      {
+        if(myFavorites[keyBar].id === key)
+        {
+          alreadyFavorited =true;
+          indexFavorited=keyBar;
+        }
+      }
+      if(alreadyFavorited)
+      {
+        myFavorites.splice(indexFavorited,1) 
+      }
+      else
+      {
+        myFavorites.push(estab)
+      }
+      console.table(myFavorites);
+      this.setState({renderlistFavorites : this.state.favoritesBar.map( (establishment) => 
+        {
+          return (
+              // L'attribut "key" permet à React d'identifier les éléments.
+              // Cela améliore les performances lors de l'ajout,
+              // la modification et la suppression d'un élément.
+              <li
+                  key = { establishment.id }
+                  className = 'establishment'
+                
+              >
+                  <h3>{ establishment.name }</h3>
+    
+              </li>
+              
+          )
+        })})
+      
+    }
   render()
   {
-    const listEstablishment = establishments.map( (establishment) => 
+    
+    //const testalacon = 50074444440;
+    this.state.renderFilteredBar = this.state.filteredBar.map( (establishment) => 
+    {
+      return (
+          // L'attribut "key" permet à React d'identifier les éléments.
+          // Cela améliore les performances lors de l'ajout,
+          // la modification et la suppression d'un élément.
+          <li
+                key = { establishment.id }
+                className = 'establishment'
+                onClick={this.changeFavorites}
+            >
+                <h3>{ establishment.name }</h3>
+  
+                { establishment.description }
+
+                <h3>Happy Hour</h3>
+  
+                { establishment.happyHour }
+  
+          </li>
+          
+      )
+    })
+    
+    /*this.state.renderlistFavorites = this.state.favoritesBar.map( (establishment) => 
     {
       return (
           // L'attribut "key" permet à React d'identifier les éléments.
@@ -21,14 +193,18 @@ class App extends React.Component
           <li
               key = { establishment.id }
               className = 'establishment'
+            
           >
               <h3>{ establishment.name }</h3>
+              {this.testalacon}
+
 
               { establishment.description }
 
           </li>
+          
       )
-    })
+    })*/
 
   return (
       <section className="App">
@@ -36,8 +212,22 @@ class App extends React.Component
               <img src={logo} className="App-logo" alt="logo" />
               <h2>Welcome to { this.props.title }</h2>
           </div>
+          <div className="App-search">
+            <input 
+            
+              type="text"
+              placeholder="find a bar"
+              value={this.state.searchString}
+              onChange={this.filterList}
+
+            />
+          </div>
           <div className="App-intro">
-              { listEstablishment }
+              { this.state.renderFilteredBar }
+          </div>
+          <div className="App-favorites">
+            <h3>Favorites Bar</h3>
+              { this.state.renderlistFavorites}
           </div>
 
       </section>
--- happydrink/src/fixtures.js
+++ happydrink/src/fixtures.js
@@ -2,16 +2,38 @@ export const establishments = [
     {
         id          : "0890786GH",
         name        : "Biberium",
-        description : "Un super bar karaoké"
+        description : "Un super bar karaoké",
+        happyHour : "17h"
     },
     {
         id          : "0890786GD",
         name        : "Brew Dogs",
-        description : "Un super bar à bière"
+        description : "Un super bar à bière",
+        happyHour : "19h"
+        
     },
     {
         id          : "MJLMH0389",
         name        : "Batonier",
-        description : "Un super bar de fin de soirée"
+        description : "Un super bar de fin de soirée",
+        happyHour : "23h"
+    },
+    {
+        id          : "AAAAAAAA",
+        name        : "Libertinage",
+        description : "Un super bar :v",
+        happyHour : "23h30"
+    },
+    {
+        id          : "XXXXXXXX",
+        name        : "X⁶",
+        description : "Un autre super bar :v",
+        happyHour : "22h"
+    },
+    {
+        id          : "ATTENTION",
+        name        : "FINAL STOP BAR",
+        description : "Le bar où finir ta soirée",
+        happyHour : "3h"
     }
 ]
